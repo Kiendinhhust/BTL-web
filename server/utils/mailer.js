00c3623b74b067cfc90280ef64c1eb02c23ed7de
Daviseraaa <trantuandoan04@gmail.com>
2025-03-27 07:40:02 +0700

    send mail modify

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'thulamhoai143@gmail.com',
-        pass: 'quqr ssrv agas liws'
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
     }
 });
 
